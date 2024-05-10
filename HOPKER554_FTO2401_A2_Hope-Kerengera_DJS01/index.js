a145cd5de17bfd31ac2952cd0ab47791326a31f2
Hope <k.hope.ing@gmail.com>
2024-05-10 13:25:51 +0200

    Gave Variables Meaningful Names, Fixed Parameters and Velocity Unit Mismatch

--- HOPKER554_FTO2401_A2_Hope-Kerengera_DJS01/index.js
+++ HOPKER554_FTO2401_A2_Hope-Kerengera_DJS01/index.js
@@ -6,29 +6,25 @@
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
+const initialVelocity = 10000; // velocity (km/h)
+const acceleration = 3; // acceleration (m/s^2)
 const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
+const distance = 0; // distance (km)
 const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
-
-const d2 = d + (vel*time) //calcultes new distance
-const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+const newDistance = distance + initialVelocity * time; //calcultes new distance
+const remainingFuel = fuelBurnRate * time; //calculates remaining fuel
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
-}
+//define calcNewVel function before calling it
+const calcNewVel = (vel, acc, time) => {
+  return vel + acc * time;
+};
 
-console.log(`Corrected New Velocity: ${vel2} km/h`);
-console.log(`Corrected New Distance: ${d2} km`);
-console.log(`Corrected Remaining Fuel: ${rf} kg`);
+//divide initialVelocity by 3.6 to make it consistent with other units (m/s), and fix the parameters
+const newVelocity = calcNewVel(initialVelocity / 3.6, acceleration, time); //calculates new velocity based on acceleration
 
-
-
-
-
-
+console.log(`Corrected New Velocity: ${newVelocity * 3.6} km/h`);
+console.log(`Corrected New Distance: ${newDistance} km`);
+console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
