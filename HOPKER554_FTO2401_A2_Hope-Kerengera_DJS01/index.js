41e1d3d849b825fc1c61e0b5a75bdfd6f3745a31
Hope <k.hope.ing@gmail.com>
2024-05-10 16:15:24 +0200

    Implemented Object Destructuring

--- HOPKER554_FTO2401_A2_Hope-Kerengera_DJS01/index.js
+++ HOPKER554_FTO2401_A2_Hope-Kerengera_DJS01/index.js
@@ -18,7 +18,7 @@ const remainingFuel = fuel - fuelBurnRate * time; //calculates remaining fuel //
 
 // Pick up an error with how the function below is called and make it robust to such errors
 //define calculateNewVelocity function before calling it
-const calculateNewVelocity = (vel, acc, time) => {
+const calculateNewVelocity = ({ vel, acc, time }) => {
   //validate input
   if (
     acc < 0 ||
@@ -28,18 +28,18 @@ const calculateNewVelocity = (vel, acc, time) => {
     isNaN(vel) ||
     isNaN(time)
   ) {
-    throw Error("All values must be positive and must be numbers");
+    throw Error("All values must be positive and must be numbers.");
   } else {
     return vel + acc * time;
   }
 };
 
 //divide initialVelocity by 3.6 to make it consistent with other units (m/s), and fix the parameters
-const newVelocity = calculateNewVelocity(
-  initialVelocity / 3.6,
-  acceleration,
-  time
-); //calculates new velocity based on acceleration
+const newVelocity = calculateNewVelocity({
+  vel: initialVelocity / 3.6,
+  acc: acceleration,
+  time: time,
+}); //calculates new velocity based on acceleration
 
 console.log(`Corrected New Velocity: ${newVelocity * 3.6} km/h`);
 console.log(`Corrected New Distance: ${newDistance / 1000} km`);
